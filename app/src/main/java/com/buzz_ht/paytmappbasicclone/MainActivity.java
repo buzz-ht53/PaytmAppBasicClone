7fcc4626a4f00b20acd93316b270fd93986c7648
buzz-ht53 <akshayr.java@gmail.com>
2021-08-24 16:48:55 +0530

    Paytm App Clone

---
+++ app/src/main/java/com/buzz_ht/paytmappbasicclone/MainActivity.java
@@ -0,0 +1,24 @@
+package com.buzz_ht.paytmappbasicclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pay(View view) {
+    }
+
+    public void sendMoney(View view) {
+    }
+
+    public void scan(View view) {
+    }
+}
